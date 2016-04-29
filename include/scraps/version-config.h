91d46ff75b1cc2f6e850e502fb22cc94db5dfcae
Mike Naquin <mike.naquin@bittorrent.com>
2016-04-28 20:22:21 -0700

    update scraps version

--- include/scraps/version-config.h
+++ include/scraps/version-config.h
@@ -3,9 +3,9 @@
 # pragma once
 
 # if !defined(SCRAPS_VERSION)
-    # define SCRAPS_VERSION "0.0.0.0"
+    # define SCRAPS_VERSION "0.1.0.0"
     # define SCRAPS_VERSION_MAJOR 0
-    # define SCRAPS_VERSION_MINOR 0
+    # define SCRAPS_VERSION_MINOR 1
     # define SCRAPS_VERSION_REVISION 0
     # define SCRAPS_VERSION_BUILD 0
 # endif
