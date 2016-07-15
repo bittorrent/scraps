a30859c6bb1406289babc85391c99f4aafae6e4e
Christopher Brown <cbrown@bittorrent.com>
2016-07-15 03:47:25 -0700

    add another warning to the ignore macro for boost

--- include/scraps/warnings.h
+++ include/scraps/warnings.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #if !SCRAPS_DISABLE_IGNORE_WARNINGS && defined(__clang__)
-    #define SCRAPS_IGNORE_WARNINGS_PUSH                           \
-        _Pragma("clang diagnostic push")                          \
-        _Pragma("clang diagnostic ignored \"-Wsign-conversion\"") \
+    #define SCRAPS_IGNORE_WARNINGS_PUSH                                \
+        _Pragma("clang diagnostic push")                               \
+        _Pragma("clang diagnostic ignored \"-Wsign-conversion\"")      \
+        _Pragma("clang diagnostic ignored \"-Wunused-local-typedef\"") \
         _Pragma("clang diagnostic ignored \"-Wconversion\"")
 
     #define SCRAPS_IGNORE_WARNINGS_POP                            \
