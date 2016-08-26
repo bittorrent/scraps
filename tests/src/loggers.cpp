b7e608dd5d191c44f46dec1518e2d72ed306b250
David Apirian <dapirian@bittorrent.com>
2016-08-26 11:23:27 -0700

    fix define names

--- tests/src/loggers.cpp
+++ tests/src/loggers.cpp
@@ -1,12 +1,12 @@
 /**
 * Copyright 2016 BitTorrent Inc.
-* 
+*
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
-* 
+*
 *    http://www.apache.org/licenses/LICENSE-2.0
-* 
+*
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,7 +25,7 @@ using namespace std::literals;
 
 TEST(loggers, DefaultLogPath) {
     const auto actual = scraps::FileLogger::DefaultLogPath("test");
-#if SCRAPS_MAC_OS_X
+#if SCRAPS_MACOS
     EXPECT_NE(actual.find("/Library/Logs/test/"), std::string::npos);
 #elif SCRAPS_LINUX
     EXPECT_EQ("/var/log/test/", actual);
