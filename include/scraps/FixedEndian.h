b75d73d6139da71fbcaed2d3b99c969cfac1a075
David Apirian <dapirian@bittorrent.com>
2016-08-26 11:45:45 -0700

    update FixedEndian for name changes

--- include/scraps/FixedEndian.h
+++ include/scraps/FixedEndian.h
@@ -1,12 +1,12 @@
 /**
 * Copyright 2016 BitTorrent Inc.
-* 
+*
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
-* 
+*
 *    http://www.apache.org/licenses/LICENSE-2.0
-* 
+*
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -39,12 +39,12 @@ private:
 
 template <typename T, Endian endian>
 constexpr FixedEndian<T, endian>::FixedEndian(T value) noexcept
-    : _value{detail::MaybeByteSwap(value, std::integral_constant<bool, Endian::kNative != endian>{})}
+    : _value{detail::MaybeSwapByteOrder(value, std::integral_constant<bool, Endian::kNative != endian>{})}
 {}
 
 template <typename T, Endian endian>
 constexpr FixedEndian<T, endian>::operator T() const noexcept {
-    return detail::MaybeByteSwap(_value, std::integral_constant<bool, Endian::kNative != endian>{});
+    return detail::MaybeSwapByteOrder(_value, std::integral_constant<bool, Endian::kNative != endian>{});
 }
 
 template <typename T>
