970f7f66113a5c51918917e3fbb591159f95879b
Christopher Brown <cbrown@bittorrent.com>
2016-09-22 02:27:24 -0700

    remove ios / tvos oes macros - don't need em anymore

--- src/opengl/VertexArray.cpp
+++ src/opengl/VertexArray.cpp
@@ -1,12 +1,12 @@
 /**
 * Copyright 2016 BitTorrent Inc.
-* 
+*
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
-* 
+*
 *    http://www.apache.org/licenses/LICENSE-2.0
-* 
+*
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,12 +17,6 @@
 
 namespace scraps {
 namespace opengl {
-
-#if SCRAPS_IOS || SCRAPS_TVOS
-#define glGenVertexArrays glGenVertexArraysOES
-#define glDeleteVertexArrays glDeleteVertexArraysOES
-#define glBindVertexArray glBindVertexArrayOES
-#endif
 
 VertexArray::VertexArray() {
 #if !SCRAPS_EMULATE_VERTEX_ARRAY
