1b7c09b7b38d6389f4cc3a44f1e5f74ca086687b
Christopher Brown <cbrown@bittorrent.com>
2016-10-14 13:51:51 -0700

    add std::array hash specialization
    Summary: add std::array hash specialization
    
    Test Plan: compile
    
    Reviewers: vmrob
    
    Reviewed By: vmrob
    
    Subscribers: #engineering
    
    Differential Revision: https://phabricator.btlive.tv/D1883

--- include/scraps/array.h
+++ include/scraps/array.h
@@ -1,12 +1,12 @@
 /**
 * Copyright 2016 BitTorrent Inc.
-* 
+*
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
-* 
+*
 *    http://www.apache.org/licenses/LICENSE-2.0
-* 
+*
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -73,3 +73,12 @@ struct ArrayHasher {
 };
 
 } // namespace scraps
+
+namespace std {
+    template<typename T, size_t N>
+    struct hash<std::array<T, N>> {
+        size_t operator()(const std::array<T, N>& array) const {
+            return scraps::ArrayHasher{}(array);
+        }
+    };
+} // namespace std
