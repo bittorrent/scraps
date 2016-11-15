cdf57dae8f887edb19b48a808ba2fadf9f07bb86
David Apirian <dapirian@bittorrent.com>
2016-11-15 09:35:56 -0800

    Add PollingFutureSynchronizer
    Summary: The same function as FutureSynchronizer, but it polls the futures when `update()` is called, and calls the callbacks synchronously. There are potential use cases for both. The use of FutureSynchronizer in MainView wasn't safe during the removal of MainView from the view hierarchy during shutdown.
    
    Test Plan: Unit tests on their way
    
    Reviewers: mike.naquin
    
    Reviewed By: mike.naquin
    
    Subscribers: #engineering
    
    Differential Revision: https://phabricator.btlive.tv/D2046

--- tests/unit/src/FutureSynchronizer.cpp
+++ tests/unit/src/FutureSynchronizer.cpp
@@ -22,7 +22,7 @@
 
 using namespace scraps;
 
-TEST(FutureSynchronizer, basicOperation) {
+TEST(FutureSynchronizer, async) {
     FutureSynchronizer fs;
 
     std::set<int> results;
@@ -39,6 +39,32 @@ TEST(FutureSynchronizer, basicOperation) {
     }
 
     std::this_thread::sleep_for(2s); // wait for FutureSynchronizer to finish up.
+
+    EXPECT_EQ(results, (std::set<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
+}
+
+TEST(FutureSynchronizer, polling) {
+    PollingFutureSynchronizer fs;
+
+    std::set<int> results;
+
+    auto mainThreadId = std::this_thread::get_id();
+
+    auto insert = [&](auto i){
+        results.insert(i);
+        EXPECT_EQ(std::this_thread::get_id(), mainThreadId);
+    };
+
+    for (auto i = 0; i < 10; ++i) {
+        fs.push(std::async([r = i]{ std::this_thread::sleep_for(100ms); return r; }), insert);
+    }
+
+    auto start = std::chrono::steady_clock::now();
+
+    while ((std::chrono::steady_clock::now()-start) < 2s && results.size() != 10) {
+        fs.update();
+        std::this_thread::sleep_for(100ms);
+    }
 
     EXPECT_EQ(results, (std::set<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
 }
